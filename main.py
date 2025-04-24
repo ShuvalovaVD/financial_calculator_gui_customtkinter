531b4749582bdac85a610146c0f6a95743702174
Shuvalova Valeria <114807973+ShuvalovaVD@users.noreply.github.com>
2025-04-24 20:30:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,63 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
-def display():
-    redraw_diagram()
+def check_day(s):
+    if s.count(".") != 2:
+        return False
+    d, m, y = s.split(".")
+    if len(m) != 2 or (not m.isdigit()):
+        return False
+    m = int(m)
+    if m < 1 or m > 12:
+        return False
+    if len(y) != 4 or (not y.isdigit()):
+        return False
+    y = int(y)
+    if y < 0:
+        return False
+    if len(d) != 2 or (not d.isdigit()):
+        return False
+    d = int(d)
+    days_in_february = 29 if (y % 400 == 0 or (y % 4 == 0 and y % 100 != 0)) else 28
+    days_in_months = {1: 31, 2: days_in_february, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30,
+                      12: 31}
+    if d < 1 or d > days_in_months[m]:
+        return False
+    return True
 
 
-def redraw_diagram():
-    global fig, ax, labels, values, canvas
+def check_day_in_period(day, start_period, finish_period):
+    dd_day, mm_day, yyyy_day = map(int, day.split("."))
+    if start_period != None:
+        dd_start, mm_start, yyyy_start = map(int, start_period.split("."))
+        if yyyy_day < yyyy_start:
+            return False
+        elif yyyy_day == yyyy_start and mm_day < mm_start:
+            return False
+        elif yyyy_day == yyyy_start and mm_day == mm_start and dd_day < dd_start:
+            return False
+    if finish_period != None:
+        dd_finish, mm_finish, yyyy_finish = map(int, finish_period.split("."))
+        if yyyy_day > yyyy_finish:
+            return False
+        elif yyyy_day == yyyy_finish and mm_day > mm_finish:
+            return False
+        elif yyyy_day == yyyy_finish and mm_day == mm_finish and dd_day > dd_finish:
+            return False
+    return True
+
+
+def report_error(error_text):
+    global my_font
+    dialog_window = ctk.CTkToplevel()
+    dialog_window.title("Ошибка")
+    dialog_window.geometry("700x100")
+    ctk.CTkLabel(master=dialog_window, text="Ошибка:", font=my_font).pack()
+    ctk.CTkLabel(master=dialog_window, text=error_text, font=my_font).pack()
+
+
+def redraw_diagram(labels, values):
+    global fig, ax, canvas
     ax.clear()  # стираем старую диаграмму
 
     # рисуем новую диаграмму
@@ -36,7 +87,88 @@ def redraw_diagram():
     ax.set_title("Распределение трат по категориям")
     # настройка отображения
     fig.tight_layout()  # автоматически корректирует отступы, чтобы ничего не накладывалось друг на друга
-    canvas.draw()
+    canvas.draw()  # отрисовываем canvas
+
+
+def redraw_window():
+    global scrollable_frame, data, my_font
+    period_info = scrollable_frame.winfo_children()[0].cget("text")
+    for widget in scrollable_frame.winfo_children():
+        widget.destroy()
+    if period_info == "За весь период:":
+        start_period = finish_period = None
+    elif period_info[:15] == "За период после":
+        start_period, finish_period = period_info[16:], None
+    elif period_info[:12] == "За период до":
+        start_period, finish_period = None, period_info[13:]
+    else:
+        start_period, finish_period = period_info[10:].split(" - ")
+
+    for widget in scrollable_frame.winfo_children():
+        widget.destroy()
+
+    ctk.CTkLabel(master=scrollable_frame, text=period_info, font=my_font).pack()
+    labels, values = [], []
+    pos = 1
+    for category, info in data.items():
+        total_price = 0
+        for day, purchase, price in info:
+            if check_day_in_period(day, start_period, finish_period):
+                total_price += price
+                if pos == 1:
+                    ctk.CTkLabel(master=scrollable_frame, text=f"{category}:", font=my_font).pack()
+                ctk.CTkLabel(master=scrollable_frame, text=f"{pos}. {day} - {purchase} - {price}р.",
+                             font=my_font).pack()
+                pos += 1
+        if total_price > 0:
+            labels.append(category)
+            values.append(total_price)
+    if labels == values == []:
+        ctk.CTkLabel(master=scrollable_frame, text="Нет трат", font=my_font).pack()
+        labels = ["Ничего"]
+        values = [100]
+
+    redraw_diagram(labels, values)
+
+
+def display():
+    global entry_start_period, entry_finish_period, scrollable_frame
+    start_period, finish_period = entry_start_period.get(), entry_finish_period.get()
+    period_info = None
+    if start_period != "" and finish_period != "":
+        if (not check_day(start_period)) or (not check_day(finish_period)):
+            report_error("Вы ввели некорректную дату")
+            return
+        dd_start, mm_start, yyyy_start = map(int, start_period.split("."))
+        dd_finish, mm_finish, yyyy_finish = map(int, finish_period.split("."))
+        if yyyy_start > yyyy_finish:
+            report_error("Вы ввели некорректную дату")
+            return
+        if yyyy_start == yyyy_finish and mm_start > mm_finish:
+            report_error("Вы ввели некорректную дату")
+            return
+        if yyyy_start == yyyy_finish and mm_start == mm_finish and dd_start > dd_finish:
+            report_error("Вы ввели некорректную дату")
+            return
+        period_info = f"За период {start_period} - {finish_period}:"
+    elif start_period != "":
+        finish_period = None
+        if not check_day(start_period):
+            report_error("Вы ввели некорректную дату")
+            return
+        period_info = f"За период после {start_period}:"
+    elif finish_period != "":
+        start_period = None
+        if not check_day(finish_period):
+            report_error("Вы ввели некорректную дату")
+            return
+        period_info = f"За период до {finish_period}:"
+    else:
+        start_period = finish_period = None
+        period_info = "За весь период:"
+
+    scrollable_frame.winfo_children()[0].configure(text=period_info)
+    redraw_window()
 
 
 def add_purchase():
@@ -46,11 +178,29 @@ def add_purchase():
     dialog_window.geometry("500x350")
 
     def add_purchase_loc():
-        pass
+        global data
+        nonlocal combobox_category, entry_day, entry_purchase, entry_price
+        category, day, purchase, price = (combobox_category.get(), entry_day.get(), entry_purchase.get(),
+                                          entry_price.get())
+        if not check_day(day):
+            report_error("Вы ввели некорректную дату")
+            return
+
+        if "-" in purchase:
+            report_error("Знака '-' не должно быть в названии покупки")
+            return
+
+        if (not price.isdigit()) or int(price) < 1:
+            report_error("Цена должна быть целым положительным числом")
+            return
+
+        data[category].append([day, purchase, price])
+
+        redraw_window()
 
     label_add = ctk.CTkLabel(master=dialog_window, text="Данные о записи:", font=my_font)
-    categories_list = ["Еда", "Жильё", "Одежда", "Проезд", "Здоровье", "Развлечения", "Быт", "Прочее"]
-    combobox_category = ctk.CTkComboBox(master=dialog_window, values=categories_list, font=my_font, state="readonly")
+    combobox_category = ctk.CTkComboBox(master=dialog_window, values=["Еда", "Жильё", "Одежда", "Проезд", "Здоровье",
+                                        "Развлечения", "Быт", "Прочее"], font=my_font, state="readonly")
     combobox_category.set("Еда")
     label_day = ctk.CTkLabel(master=dialog_window, text="День:", font=my_font)
     entry_day = ctk.CTkEntry(master=dialog_window, placeholder_text="дд.мм.гггг", font=my_font, justify="center",
@@ -87,10 +237,34 @@ def delete_purchase():
     dialog_window.geometry("300x200")
 
     def delete_purchase_loc():
-        pass
+        global scrollable_frame, data
+        nonlocal entry_delete
+        answer = entry_delete.get()
+        if not answer.isdigit():
+            report_error("Вы ввели что-то отличное от целого числа")
+            return
+
+        category = None
+        flag_is_del = False
+        for widget in scrollable_frame.winfo_children()[1:]:
+            if "." not in widget.cget("text"):
+                category = widget.cget("text")[:-1]
+                continue
+            if widget.cget("text").split(".")[0] == answer:
+                day, purchase, price = widget.cget("text")[len(answer) + 2:].split(" - ")
+                price = int(price[:-2])
+                data[category].remove([day, purchase, price])
+                flag_is_del = True
+                break
+
+        if not flag_is_del:
+            report_error("Пункта с данным номером нет в истории трат")
+            return
+
+        redraw_window()
 
     label_delete = ctk.CTkLabel(master=dialog_window, text="Номер записи:", font=my_font)
-    entry_delete = ctk.CTkEntry(master=dialog_window, font=my_font)
+    entry_delete = ctk.CTkEntry(master=dialog_window, font=my_font, justify="center")
     button_delete = ctk.CTkButton(master=dialog_window, text="Удалить", font=my_font, command=delete_purchase_loc)
     rows, columns = 3, 1
     for i in range(rows):
@@ -109,13 +283,37 @@ def clear_all():
     dialog_window.geometry("300x200")
 
     def answer_yes():
-        pass
+        global scrollable_frame, data, my_font
+        period_info = scrollable_frame.winfo_children()[0].cget("text")
+        for widget in scrollable_frame.winfo_children():
+            widget.destroy()
+        if period_info == "За весь период:":
+            start_period = finish_period = None
+        elif period_info[:15] == "За период после":
+            start_period, finish_period = period_info[16:-1], None
+        elif period_info[:12] == "За период до":
+            start_period, finish_period = None, period_info[13:-1]
+        else:
+            start_period, finish_period = period_info[10:-1].split(" - ")
+
+        for category, info in data.items():
+            elems_to_del = []
+            for day, purchase, price in info:
+                if check_day_in_period(day, start_period, finish_period):
+                    elems_to_del.append([day, purchase, price])
+            for elem in elems_to_del:
+                data[category].remove(elem)
+
+        ctk.CTkLabel(master=scrollable_frame, text=period_info, font=my_font).pack()
+        ctk.CTkLabel(master=scrollable_frame, text="Нет трат", font=my_font).pack()
+        redraw_diagram(["Ничего"], [100])
+        dialog_window.destroy()
 
     def answer_no():
         dialog_window.destroy()
 
-    label_clear = ctk.CTkLabel(master=dialog_window, text="Уверены, что хотите\nудалить все записи\nза указанный период?",
-                               font=my_font)
+    label_clear = ctk.CTkLabel(master=dialog_window,
+                               text="Уверены, что хотите\nудалить все записи\nза указанный период?", font=my_font)
     button_yes = ctk.CTkButton(master=dialog_window, text="Да", font=my_font, command=answer_yes)
     button_no = ctk.CTkButton(master=dialog_window, text="Нет", font=my_font, command=answer_no)
     rows, columns = 2, 2
@@ -130,7 +328,7 @@ def clear_all():
 
 # перед началом работы приложения загружаем данные
 # в файле data.json данные записаны в словарь, где ключи - категории, значения - списки, которые имеют формат:
-# ["дата", "покупка", цена], причем даты расположены в хронологическом порядке
+# ["дата", "покупка", цена]
 # таким образом, data.json содержит всю историю трат, распределённую по категориям
 with open(file="data.json", mode="r", encoding="utf-8") as file_in:
     data = json.load(file_in)
@@ -196,18 +394,23 @@ ax = fig.add_subplot(111)
 # встраиваем фигуру fig в рамку frame с помощью canvas
 canvas = FigureCanvasTkAgg(fig, master=frame_for_diagram)
 canvas.get_tk_widget().pack(fill="both", expand=True)
-redraw_diagram()  # на старте отрисовываем диаграмму
+# на старте отрисовываем диаграмму
+redraw_diagram(labels, values)
 
 # на старте отображаем загруженную историю трат за весь период
-current_history = []
-for category, info in data.items():
-    if len(info) == 0:
-        continue
-    ctk.CTkLabel(master=scrollable_frame, text=f"{category}:", font=my_font).pack()
-    for day, purchase, price in info:
-        current_history.append((category, day, purchase, price))
-        ctk.CTkLabel(master=scrollable_frame,
-                     text=f"{len(current_history)}. {day} - {purchase} - {price}р.", font=my_font).pack()
+ctk.CTkLabel(master=scrollable_frame, text="За весь период:", font=my_font).pack()
+if labels == ["Ничего"]:
+    ctk.CTkLabel(master=scrollable_frame, text="Нет трат", font=my_font).pack()
+else:
+    pos = 1
+    for category, info in data.items():
+        if len(info) == 0:
+            continue
+        ctk.CTkLabel(master=scrollable_frame, text=f"{category}:", font=my_font).pack()
+        for day, purchase, price in info:
+            ctk.CTkLabel(master=scrollable_frame,
+                         text=f"{pos}. {day} - {purchase} - {price}р.", font=my_font).pack()
+            pos += 1
 
 plt.close()  # завершает процессы, связанные с графиками
 root.mainloop()
